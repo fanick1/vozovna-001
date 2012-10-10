8bee4beefb6e47ce0f2ff180ed9e3eaf629dbf4a
František Veverka <Frantiek Veverka@70db60b9-7467-62f0-d0e7-3f4e42a13e68>
2012-10-10 19:40:37 +0000

    ADDED: src/main/java/cz/muni/fi/pa65/vozovna/entities/User.java - User entity

---
+++ trunk/src/main/java/cz/muni/fi/pa165/vozovna/entities/User.java
@@ -0,0 +1,168 @@
+/**
+ *  Copyright 2012 Frantisek Veverka, Eva Neduchalova, Jozef Triscik, Lukas Hajek
+ *  The latest (and the greatest) version of this software can be obtained at 
+ *	http://code.google.com/p/vozovna-001/
+ *
+ *  This file is part of vozovna.
+ *   vozovna is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  vozovna is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with vozovna.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package cz.muni.fi.pa165.vozovna.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * The User entity this class represents any user present in the application
+ * 
+ * @author Frantisek Veverka, 207422@mail.muni.cz
+ */
+@Entity
+public class User {
+
+	/**
+	 * Unique users ID
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	/**
+	 * Users name (first name + surname)
+	 */
+	@Column(length = 50, nullable = false)
+	private String name;
+
+	/**
+	 * User's category - important when deciding which vehicles can be reserved
+	 */
+	@Column(nullable = false)
+	private int userClassId;
+
+	/**
+	 * True when this user is administrator
+	 */
+	@Column(nullable = false)
+	private Boolean isAdmin;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return this.id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return this.name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the userClassId
+	 */
+	public int getUserClassId() {
+		return this.userClassId;
+	}
+
+	/**
+	 * @param userClassId
+	 *            the userClassId to set
+	 */
+	public void setUserClassId(int userClassId) {
+		this.userClassId = userClassId;
+	}
+
+	/**
+	 * @return the isAdmin
+	 */
+	public Boolean getIsAdmin() {
+		return this.isAdmin;
+	}
+
+	/**
+	 * @param isAdmin
+	 *            the isAdmin to set
+	 */
+	public void setIsAdmin(Boolean isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null || other.id == null){ //can't compare null ids
+			return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("User [id=");
+		builder.append(id);
+		builder.append(", name=");
+		builder.append(name);
+		builder.append(", userClassId=");
+		builder.append(userClassId);
+		builder.append(", isAdmin=");
+		builder.append(isAdmin);
+		builder.append("]");
+		return builder.toString();
+	}
+}
