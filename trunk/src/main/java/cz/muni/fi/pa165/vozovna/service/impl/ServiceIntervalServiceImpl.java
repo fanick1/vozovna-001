0aae96b8dc0348e198bf4bf2afbcbc38ea9c7632
Lukáš Hájek <Luk Hjek@70db60b9-7467-62f0-d0e7-3f4e42a13e68>
2012-11-01 13:26:14 +0000

    Implementation of services. Comment of duplicate dependency (hibernate-jpa-2.0-api) in pom.xml

--- trunk/src/main/java/cz/muni/fi/pa165/vozovna/service/impl/ServiceIntervalServiceImpl.java
+++ trunk/src/main/java/cz/muni/fi/pa165/vozovna/service/impl/ServiceIntervalServiceImpl.java
@@ -1,43 +1,85 @@
 package cz.muni.fi.pa165.vozovna.service.impl;
 
+import cz.muni.fi.pa165.vozovna.dao.ServiceIntervalDAO;
 import cz.muni.fi.pa165.vozovna.dto.ServiceIntervalDTO;
 import cz.muni.fi.pa165.vozovna.dto.VehicleDTO;
+import cz.muni.fi.pa165.vozovna.entity.ServiceInterval;
 import cz.muni.fi.pa165.vozovna.service.ServiceIntervalService;
+import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
- *
+ * Implementation of ServiceInterval's service
+ * 
+ * @author Lukas Hajek <359617@mail.muni.cz>
  */
 public class ServiceIntervalServiceImpl implements ServiceIntervalService {
 
+    private ServiceIntervalDAO serviceIntervalDAO;
+
+    @Autowired
+    public void setUserDAO(ServiceIntervalDAO serviceIntervalDAO) {
+        this.serviceIntervalDAO = serviceIntervalDAO;
+    }
+    
     @Override
+    @Transactional(readOnly=true)
     public ServiceIntervalDTO getById(Long id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        ServiceInterval interval = serviceIntervalDAO.getById(id);
+        ServiceIntervalDTO serviceIntervalDTO = new ServiceIntervalDTO();
+        serviceIntervalDTO.fromServiceInterval(interval);
+        
+        return serviceIntervalDTO;
     }
 
     @Override
+    @Transactional
     public void create(ServiceIntervalDTO serviceInterval) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        serviceIntervalDAO.create(serviceInterval.toServiceInterval());
     }
 
     @Override
+    @Transactional
     public void remove(ServiceIntervalDTO serviceInterval) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        serviceIntervalDAO.remove(serviceInterval.toServiceInterval());
     }
 
     @Override
+    @Transactional
     public void update(ServiceIntervalDTO serviceInterval) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        serviceIntervalDAO.update(serviceInterval.toServiceInterval());
     }
 
     @Override
+    @Transactional(readOnly=true)
     public List<ServiceIntervalDTO> findAll() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        List<ServiceInterval> intervals = serviceIntervalDAO.findAll();
+        
+        List<ServiceIntervalDTO> result = new ArrayList<>();
+        for(ServiceInterval serviceInterval : intervals) {
+            ServiceIntervalDTO dto = new ServiceIntervalDTO();
+            dto.fromServiceInterval(serviceInterval);
+            result.add(dto);
+        }
+        
+        return result;
     }
 
     @Override
+    @Transactional(readOnly=true)
     public List<ServiceIntervalDTO> findAllByVehicle(VehicleDTO vehicle) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        List<ServiceInterval> intervals = serviceIntervalDAO.findAllByVehicle(vehicle.toVehicle());
+        
+        List<ServiceIntervalDTO> result = new ArrayList<>();
+        for(ServiceInterval serviceInterval : intervals) {
+            ServiceIntervalDTO dto = new ServiceIntervalDTO();
+            dto.fromServiceInterval(serviceInterval);
+            result.add(dto);
+        }
+        
+        return result;
     }
     
 }
--- trunk/src/main/java/cz/muni/fi/pa165/vozovna/service/impl/UserServiceImpl.java
+++ trunk/src/main/java/cz/muni/fi/pa165/vozovna/service/impl/UserServiceImpl.java
@@ -1,9 +1,14 @@
 package cz.muni.fi.pa165.vozovna.service.impl;
 
+import cz.muni.fi.pa165.vozovna.dao.UserDAO;
 import cz.muni.fi.pa165.vozovna.dto.UserDTO;
+import cz.muni.fi.pa165.vozovna.entity.User;
 import cz.muni.fi.pa165.vozovna.service.UserService;
+import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 
@@ -12,34 +17,64 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl implements UserService {
 
+    private UserDAO userDAO;
+
+    @Autowired
+    public void setUserDAO(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+    
     @Override
+    @Transactional(readOnly = true)
     public UserDTO getById(Long id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        User user = userDAO.getById(id);
+        UserDTO userDTO = new UserDTO();
+        userDTO.fromUser(user);
+        return  userDTO;
     }
 
     @Override
+    @Transactional
     public void create(UserDTO user) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        userDAO.create(user.toUser());
     }
 
     @Override
+    @Transactional
     public void remove(UserDTO user) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        userDAO.remove(user.toUser());
     }
 
     @Override
+    @Transactional
     public void update(UserDTO user) {
-        throw new UnsupportedOperationException("Not supported yet.");
+       userDAO.update(user.toUser());
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<UserDTO> findAll() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        List<User> users = userDAO.findAll();
+        List<UserDTO> userDTOs = new ArrayList<>();
+        for(User u:users) {
+            UserDTO dto = new UserDTO();
+            dto.fromUser(u);
+            userDTOs.add(dto);
+        }
+        return userDTOs;
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<UserDTO> findByLastName(String lastName) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        List<User> users = userDAO.findByLastName(lastName);
+        List<UserDTO> userDTOs = new ArrayList<>();
+        for(User u:users) {
+            UserDTO dto = new UserDTO();
+            dto.fromUser(u);
+            userDTOs.add(dto);
+        }
+        return userDTOs;
     }
 
    
