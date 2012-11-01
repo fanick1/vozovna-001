fb61924b70ee25a302d04f4cbbf03877ca28bc3f
Lukáš Hájek <Luk Hjek@70db60b9-7467-62f0-d0e7-3f4e42a13e68>
2012-11-01 13:26:14 +0000

    Implementation of services. Comment of duplicate dependency (hibernate-jpa-2.0-api) in pom.xml

--- src/main/java/cz/muni/fi/pa165/vozovna/service/impl/DriveServiceImpl.java
+++ src/main/java/cz/muni/fi/pa165/vozovna/service/impl/DriveServiceImpl.java
@@ -1,53 +1,89 @@
 package cz.muni.fi.pa165.vozovna.service.impl;
 
-import java.util.List;
-
+import cz.muni.fi.pa165.vozovna.dao.DriveDAO;
 import cz.muni.fi.pa165.vozovna.dto.DriveDTO;
 import cz.muni.fi.pa165.vozovna.dto.UserDTO;
+import cz.muni.fi.pa165.vozovna.entity.Drive;
 import cz.muni.fi.pa165.vozovna.service.DriveService;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
- *
- * @author Eva Neduchalová, učo 359893
+ * Implementation of Drive service.
+ * 
+ * @author Lukas Hajek <359617@mail.muni.cz>
  */
 public class DriveServiceImpl implements DriveService {
 
+    private DriveDAO driveDAO;
+
+    @Autowired
+    public void setDriveDAO(DriveDAO driveDAO) {
+        this.driveDAO = driveDAO;
+    }
+    
     @Override
+    @Transactional(readOnly=true)
     public DriveDTO getById(Long id) {
-        // TODO Auto-generated method stub
-        return null;
+        Drive drive = driveDAO.getById(id);
+        
+        DriveDTO dto = new DriveDTO();
+        dto.fromDrive(drive);
+        
+        return dto;
     }
 
     @Override
+    @Transactional
     public void create(DriveDTO drive) {
-        // TODO Auto-generated method stub
-        
+        driveDAO.create(drive.toDrive());        
     }
 
     @Override
+    @Transactional
     public void remove(DriveDTO drive) {
-        // TODO Auto-generated method stub
-        
+        driveDAO.remove(drive.toDrive()); 
     }
 
     @Override
+    @Transactional
     public void update(DriveDTO drive) {
-        // TODO Auto-generated method stub
-        
+       driveDAO.update(drive.toDrive()); 
     }
 
     @Override
+    @Transactional(readOnly=true)
     public List<DriveDTO> findAll() {
-        // TODO Auto-generated method stub
-        return null;
+        List<Drive> drives = driveDAO.findAll();
+        
+        return convertListOfDrivesToListOfDriveDTOs(drives); 
     }
 
     @Override
+    @Transactional(readOnly=true)
     public List<DriveDTO> findByUser(UserDTO user) {
-        // TODO Auto-generated method stub
-        return null;
+        List<Drive> drives = driveDAO.findByUser(user.toUser());
+        
+        return convertListOfDrivesToListOfDriveDTOs(drives); 
     }
 
-   
+    /**
+     * Converts list of drives to list of drive DTOs
+     * @param list      List of drives
+     * @return          List of Drive Data Transform Objects
+     */
+    private static List<DriveDTO> convertListOfDrivesToListOfDriveDTOs(List <Drive> drives) {
+        List<DriveDTO> result = new ArrayList<>();
+        
+        for(Drive drive : drives) {
+            DriveDTO dto = new DriveDTO();
+            dto.fromDrive(drive);
+            result.add(dto);
+        }
+        
+        return result; 
+   }
     
 }
--- src/main/java/cz/muni/fi/pa165/vozovna/service/impl/VehicleServiceImpl.java
+++ src/main/java/cz/muni/fi/pa165/vozovna/service/impl/VehicleServiceImpl.java
@@ -1,43 +1,88 @@
 package cz.muni.fi.pa165.vozovna.service.impl;
 
+import cz.muni.fi.pa165.vozovna.dao.VehicleDAO;
 import cz.muni.fi.pa165.vozovna.dto.VehicleDTO;
+import cz.muni.fi.pa165.vozovna.entity.Vehicle;
 import cz.muni.fi.pa165.vozovna.enums.UserClassEnum;
 import cz.muni.fi.pa165.vozovna.service.VehicleService;
+import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
- *
+ * 
+ * @author Lukas Hajek <359617@mail.muni.cz>
  */
 public class VehicleServiceImpl implements VehicleService {
 
+    private VehicleDAO vehicleDAO;
+
+    @Autowired
+    public void setUserDAO(VehicleDAO vehicleDAO) {
+        this.vehicleDAO = vehicleDAO;
+    }
+    
     @Override
+    @Transactional(readOnly=true)
     public VehicleDTO getById(Long id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Vehicle vehicle = vehicleDAO.getById(id);
+        
+        VehicleDTO vehicleDTO = new VehicleDTO();
+        vehicleDTO.fromVehicle(vehicle);
+        
+        return vehicleDTO;
     }
 
     @Override
+    @Transactional
     public void create(VehicleDTO vehicle) {
-        throw new UnsupportedOperationException("Not supported yet.");
+       vehicleDAO.create(vehicle.toVehicle());
     }
 
     @Override
+    @Transactional
     public void remove(VehicleDTO vehicle) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        vehicleDAO.remove(vehicle.toVehicle());
     }
 
     @Override
+    @Transactional
     public void update(VehicleDTO vehicle) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        vehicleDAO.update(vehicle.toVehicle());
     }
 
     @Override
+    @Transactional(readOnly=true)
     public List<VehicleDTO> findAll() {
-        throw new UnsupportedOperationException("Not supported yet.");
+      
+        List<Vehicle> vehicles = vehicleDAO.findAll();
+        
+        // transform from List<Vehicle> to List<VehicleDTO>
+        List<VehicleDTO> result = new ArrayList<>();
+        for(Vehicle vehicle:vehicles) {
+            VehicleDTO dto = new VehicleDTO();
+            dto.fromVehicle(vehicle);
+            result.add(dto);
+        }
+        
+        return result;
     }
 
     @Override
+    @Transactional(readOnly=true)
     public List<VehicleDTO> findByUserClass(UserClassEnum userClass) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        
+        List<Vehicle> vehicles = vehicleDAO.findByUserClass(userClass);
+        
+        List<VehicleDTO> result = new ArrayList<>();
+        for(Vehicle vehicle:vehicles) {
+            VehicleDTO dto = new VehicleDTO();
+            dto.fromVehicle(vehicle);
+            result.add(dto);
+        }
+        
+        return result;
     }
 
    
