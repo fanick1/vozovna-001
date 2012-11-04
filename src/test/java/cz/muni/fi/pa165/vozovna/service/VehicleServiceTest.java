837b7e8f4d898ece543f054e6ea80d4d8cdfc372
František Veverka <Frantiek Veverka@70db60b9-7467-62f0-d0e7-3f4e42a13e68>
2012-11-04 23:04:32 +0000

    - Service layer tests added (DriveServiceTest functional) - added Mockito dependency - FIXED: DriveDTO.equals()

---
+++ src/test/java/cz/muni/fi/pa165/vozovna/service/VehicleServiceTest.java
@@ -0,0 +1,44 @@
+package cz.muni.fi.pa165.vozovna.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("VehicleServiceTest-context.xml")
+public class VehicleServiceTest {
+
+	@Test
+	public void testGetById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindByUserClass() {
+		fail("Not yet implemented");
+	}
+
+}
