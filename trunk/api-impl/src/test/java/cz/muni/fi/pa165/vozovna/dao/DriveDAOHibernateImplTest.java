e30f0b9ae211cf39f66dfd40ca21a3235f0cd1ef
fanickbux@gmail.com <fanickbux@gmail.com@70db60b9-7467-62f0-d0e7-3f4e42a13e68>
2012-11-26 02:14:31 +0000

    Project separated into 3 maven modules - api, api-impl and webapp. Tests not working.

---
+++ trunk/api-impl/src/test/java/cz/muni/fi/pa165/vozovna/dao/DriveDAOHibernateImplTest.java
@@ -0,0 +1,7 @@
+package cz.muni.fi.pa165.vozovna.dao;
+
+import cz.muni.fi.pa165.vozovna.dao.DriveDAOTest;
+
+public class DriveDAOHibernateImplTest extends DriveDAOTest {
+
+}
---
+++ trunk/webapp/src/main/java/cz/muni/fi/pa165/vozovna/validators/VehicleValidator.java
@@ -0,0 +1,66 @@
+package cz.muni.fi.pa165.vozovna.validators;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import cz.muni.fi.pa165.vozovna.dto.VehicleDTO;
+
+/**
+ *
+ * @author Lukas Hajek <359617@mail.muni.cz>
+ */
+public class VehicleValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return VehicleDTO.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object obj, Errors errors) {
+
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "brand", "error.vehicle.brand");
+        ValidationUtils.rejectIfEmpty(errors, "distanceCount", "error.vehicle.distanceCount");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "engineType", "error.vehicle.engineType");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "type", "error.vehicle.type");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "vin", "error.vehicle.vin");
+        ValidationUtils.rejectIfEmpty(errors, "yearMade", "error.vehicle.yearMade");
+        ValidationUtils.rejectIfEmpty(errors, "userClass", "error.vehicle.userClass");
+
+        VehicleDTO vehicle = (VehicleDTO) obj;
+
+        // validate yearMade
+        if(vehicle.getYearMade() < 1900) {
+            errors.rejectValue("yearMade", "error.vehicle.yearMade.min");
+        }
+        // validate yearMade
+        if(vehicle.getYearMade() > 2100) {
+            errors.rejectValue("yearMade", "error.vehicle.yearMade.max");
+        }
+        // validate yearMade
+        if(vehicle.getDistanceCount() < 0) {
+            errors.rejectValue("distanceCount", "error.vehicle.distanceCount.lsThanZero");
+        }
+        
+        // brand
+        if (vehicle.getBrand().length() > 20) {
+            errors.rejectValue("brand", "error.vehicle.brand.maxLength");
+        }
+        // type
+        if (vehicle.getType().length() > 40) {
+            errors.rejectValue("type", "error.vehicle.type.maxLength");
+        }
+        // engineType
+        if (vehicle.getEngineType().length() > 20) {
+            errors.rejectValue("engineType", "error.vehicle.engineType.maxLength");
+        }
+        
+        // vin
+        if (vehicle.getVin().length() > 17) {
+            errors.rejectValue("vin", "error.vehicle.vin.maxLength");
+        }
+
+    }
+
+}
