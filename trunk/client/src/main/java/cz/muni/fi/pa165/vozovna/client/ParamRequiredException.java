7ee01d3fb57a71539a931518bfea0effaa78a5ff
fanickbux@gmail.com <fanickbux@gmail.com@70db60b9-7467-62f0-d0e7-3f4e42a13e68>
2013-01-23 01:56:09 +0000

    UTF-8 inconsistencies hopefully fixed once for all (compilation should fail since now if a file contains non-UTF-8 chars). Fixed webservice client README file.

--- trunk/client/src/main/java/cz/muni/fi/pa165/vozovna/client/ParamRequiredException.java
+++ trunk/client/src/main/java/cz/muni/fi/pa165/vozovna/client/ParamRequiredException.java
@@ -2,29 +2,25 @@ package cz.muni.fi.pa165.vozovna.client;
 
 public class ParamRequiredException extends RuntimeException {
 
+	private static final long serialVersionUID = -7019105658779864656L;
+
 	public ParamRequiredException() {
-		// TODO Auto-generated constructor stub
 	}
 
 	public ParamRequiredException(String arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 
 	public ParamRequiredException(Throwable arg0) {
 		super(arg0);
-		// TODO Auto-generated constructor stub
 	}
 
 	public ParamRequiredException(String arg0, Throwable arg1) {
 		super(arg0, arg1);
-		// TODO Auto-generated constructor stub
 	}
-
 	public ParamRequiredException(String arg0, Throwable arg1, boolean arg2,
 			boolean arg3) {
 		super(arg0, arg1, arg2, arg3);
-		// TODO Auto-generated constructor stub
 	}
 
 }
