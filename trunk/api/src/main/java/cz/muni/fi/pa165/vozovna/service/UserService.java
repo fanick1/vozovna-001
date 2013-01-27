7b3184a14c371873409db75007db198e91965870
borec_Loretta@centrum.cz <borec_Loretta@centrum.cz@70db60b9-7467-62f0-d0e7-3f4e42a13e68>
2013-01-27 12:26:03 +0000

    refactoring: code cleaning, comments translation

--- trunk/api/src/main/java/cz/muni/fi/pa165/vozovna/service/UserService.java
+++ trunk/api/src/main/java/cz/muni/fi/pa165/vozovna/service/UserService.java
@@ -1,47 +1,50 @@
 package cz.muni.fi.pa165.vozovna.service;
 
-import cz.muni.fi.pa165.vozovna.dto.UserDTO;
 import java.util.List;
 
+import cz.muni.fi.pa165.vozovna.dto.UserDTO;
+
 /**
- * User Service 
+ * User Service
+ * 
  * @author Lukas Hajek <359617@mail.muni.cz>
  */
 public interface UserService {
+
     /**
      * Returns User with given id. Returns null if user does not exists.
      * 
-     * @param id        ID of user
-     * @return          UserDTO if User with given id exists, null otherwise.
-     * @throws IllegalArgumentException     Throws if given id is null.
-     * @throws DataAccessException          Throws if a data access exception occurred.
+     * @param id ID of user
+     * @return UserDTO if User with given id exists, null otherwise.
+     * @throws IllegalArgumentException Throws if given id is null.
+     * @throws DataAccessException Throws if a data access exception occurred.
      */
     public UserDTO getById(Long id);
 
     /**
      * Saves given user into database.
      * 
-     * @param user      User to save
-     * @throws IllegalArgumentException     Throws if given user is null.
-     * @throws DataAccessException          Throws if a data access exception occurred.
+     * @param user User to save
+     * @throws IllegalArgumentException Throws if given user is null.
+     * @throws DataAccessException Throws if a data access exception occurred.
      */
     public Long create(UserDTO user);
 
     /**
      * Removes given user from database.
      * 
-     * @param user      User to remove
-     * @throws IllegalArgumentException     Throws if given user is null.
-     * @throws DataAccessException          Throws if a data access exception occurred.
+     * @param user User to remove
+     * @throws IllegalArgumentException Throws if given user is null.
+     * @throws DataAccessException Throws if a data access exception occurred.
      */
     public void remove(UserDTO user);
 
     /**
      * Updates given user in database.
      * 
-     * @param user      User to update
-     * @throws IllegalArgumentException     Throws if given user is null.
-     * @throws DataAccessException          Throws if a data access exception occurred.
+     * @param user User to update
+     * @throws IllegalArgumentException Throws if given user is null.
+     * @throws DataAccessException Throws if a data access exception occurred.
      */
     public UserDTO update(UserDTO user);
 
@@ -49,17 +52,17 @@ public interface UserService {
      * Find and return all users in database.
      * 
      * @return List of all users.
-     * @throws DataAccessException          Throws if a data access exception occurred.
+     * @throws DataAccessException Throws if a data access exception occurred.
      */
     public List<UserDTO> findAll();
 
     /**
      * Find and return users with given username.
      * 
-     * @param username      username.
+     * @param username username.
      * @return userDTO of user with given username
-     * @throws IllegalArgumentException     Throws if given username is null.
-     * @throws DataAccessException          Throws if a data access exception occurred.
+     * @throws IllegalArgumentException Throws if given username is null.
+     * @throws DataAccessException Throws if a data access exception occurred.
      */
     public UserDTO getByUsername(String username);
 
