96bf6d6f64e65d754a28ca23fe634d3a71940b2c
fanickbux@gmail.com <fanickbux@gmail.com@70db60b9-7467-62f0-d0e7-3f4e42a13e68>
2013-01-27 13:40:45 +0000

    PasswordCallbacks for webservices cleaned and commented

--- trunk/ws/src/main/java/cz/muni/fi/pa165/vozovna/ws/PasswordCallback.java
+++ trunk/ws/src/main/java/cz/muni/fi/pa165/vozovna/ws/PasswordCallback.java
@@ -2,6 +2,7 @@ package cz.muni.fi.pa165.vozovna.ws;
 
 import java.io.IOException;
 
+
 import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.callback.UnsupportedCallbackException;
@@ -9,21 +10,22 @@ import javax.security.auth.callback.UnsupportedCallbackException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.encoding.PasswordEncoder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import org.apache.ws.security.WSPasswordCallback;
+
+/**
+ * Callback for authenticating web-service clients
+ * @author Frantisek Veverka, 207422@mail.muni.cz
+ */
 public class PasswordCallback implements CallbackHandler {
 	
 	private static final Logger LGR = LoggerFactory.getLogger(PasswordCallback.class);
 	
 	@Autowired 
 	private UserDetailsService userDetailsService;
-	
-	@Autowired
-	private PasswordEncoder passwordEncoder;
-	
+		
 	@Override
 	public void handle(Callback[] callbacks) throws IOException,
 			UnsupportedCallbackException {
@@ -43,13 +45,10 @@ public class PasswordCallback implements CallbackHandler {
 			 if (pc.getUsage() == WSPasswordCallback.USERNAME_TOKEN) {
 				 UserDetails ud = userDetailsService.loadUserByUsername(pc.getIdentifier());
 				 if(ud != null){
-					 LGR.warn("Prihlasovaci jmeno a heslo {} {} ",pc.getIdentifier(), pc.getPassword());
-					 
 					 pc.setPassword(ud.getPassword());
-					 LGR.warn("Heslo v databazi {} ",ud.getPassword());
 				 }
 			 }
 		 }
-		 LGR.warn("PasswordCallback ended.");
+		 LGR.trace("PasswordCallback ended.");
 	}		
 }
